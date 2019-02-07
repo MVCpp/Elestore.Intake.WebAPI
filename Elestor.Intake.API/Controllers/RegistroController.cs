1944b599ba63d3e17dbfbf2f018aa2d4e844fab5
miguelvazquez <miguel.cepepe@hotmail.com>
2019-02-07 15:35:52 -0600

    * .gitignore: API build
    * Elestor.Intake.API.sln:
    * Program.cs:
    * Startup.cs:
    * Class1.cs:
    * MyClass.cs:
    * EmptyClass.cs:
    * Elestor.Intake.API.csproj:
    * launchSettings.json:
    * AssemblyInfo.cs:
    * RegistroController.cs:
    * Elestor.Intake.Contracts.csproj:

---
+++ Elestor.Intake.API/Controllers/RegistroController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Elestor.Intake.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elestor.Intake.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/registro")]
+    public class RegistroController : Controller
+    {
+        public RegistroController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult cadena(string cad)
+        {
+            EmptyClass emptyClass = new EmptyClass();
+
+            emptyClass.cadena = "Hola";
+            return Ok(emptyClass);
+        }
+    }
+}
---
+++ Elestor.Intake.API/Models/EmptyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Elestor.Intake.API.Models
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+           
+        }
+
+        public string cadena { get; set; } = string.Empty; 
+    }
+}
