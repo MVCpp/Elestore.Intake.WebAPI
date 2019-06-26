30b2d9b9efbeaa5fa28bee0f1a33ddb4fa14d6c1
mvcpp <miguel.cepepe@hotmail.com>
2019-06-26 14:57:01 -0500

    Update StringToBase64.cs
    Base64ToString and vice-versa

--- Elestor.Intake.API/Helpers/StringToBase64.cs
+++ Elestor.Intake.API/Helpers/StringToBase64.cs
@@ -10,10 +10,14 @@ namespace Elestor.Intake.API.Helpers
         {
            return Encoding.ASCII.GetBytes(str); ;
         }
+        
+        //System.Text.Encoding.UTF8.GetString(System.Convert.FromBase64String(base64EncodedData));
 
         public static string GetString(this byte[] bytes)
         {
             return Encoding.ASCII.GetString(bytes, 0, bytes.Length);
         }
+        
+         //System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(plainTextBytes));
     }
 }
