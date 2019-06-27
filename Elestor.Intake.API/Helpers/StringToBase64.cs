1d71fead8e6cfefe82f0ede4734982ea9bc7192f
Miguel Vazquez <miguel.cepepe@hotmail.com>
2019-06-26 19:58:44 -0500

    base64

--- Elestor.Intake.API/Helpers/StringToBase64.cs
+++ Elestor.Intake.API/Helpers/StringToBase64.cs
@@ -8,16 +8,16 @@ namespace Elestor.Intake.API.Helpers
      
         public static byte[] GetBytes(this string str)
         {
-            return System.Convert.FromBase64String(str);
-            //return Encoding.ASCII.GetBytes(str); ;
+            //return System.Convert.FromBase64String(str);
+            return Encoding.UTF8.GetBytes(str);
         }
         
         //System.Text.Encoding.UTF8.GetString(System.Convert.FromBase64String(base64EncodedData));
 
         public static string GetString(this byte[] bytes)
         {
-            return System.Convert.ToBase64String(bytes);
-            //return Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+            //return System.Convert.ToBase64String(bytes);
+            return Encoding.UTF8.GetString(bytes, 0, bytes.Length);
         }
         
          //System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(plainTextBytes));
