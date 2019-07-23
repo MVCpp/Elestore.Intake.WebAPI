15419f49e7e634bd3fc2db30e05c6062efa9ae72
Miguel Vazquez <Miguel.Vazquez@verscend.com>
2019-07-23 15:56:52 -0500

    Generic Response

---
+++ Elestor.Intake.API/Helpers/Response.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+
+namespace Elestor.Intake.API.Helpers
+{
+    public class Response<T>
+    {
+        private T _data;
+        private bool _success = true;
+        private string _message;
+
+        [DataMember()]
+        public bool success
+        {
+            get { return _success; }
+            set { _success = value; }
+        }
+
+        [DataMember()]
+        public string message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+        [DataMember()]
+        public T data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+        public Response(ref T data)
+        {
+            _data = data;
+        }
+    }
+}
